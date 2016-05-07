6777d4a1fcb5fdca79b1d0009f07c22766848765
dxspasojevic <dxspasojevic@agiledigital.com.au>
2016-05-08 00:46:33 +1000

    Starting clean-up.

--- modules/core/src/main/scala/au/com/agiledigital/dao/slick/SoftDeleteActions.scala
+++ modules/core/src/main/scala/au/com/agiledigital/dao/slick/SoftDeleteActions.scala
@@ -5,7 +5,7 @@ import slick.ast.BaseTypedType
 import scala.concurrent.ExecutionContext
 
 /**
-  *
+  * Support for soft-deleting entities rather than deleting the rows.
   */
 trait SoftDeleteActions[Entity, PendingEntity] extends EntityActions[Entity, PendingEntity] {
 
@@ -15,18 +15,30 @@ trait SoftDeleteActions[Entity, PendingEntity] extends EntityActions[Entity, Pen
 
   protected implicit lazy val recordStatusBasedType: BaseTypedType[RecordStatus] = baseRecordStatusTypedType
 
+  /**
+    * Type of the field that will indicate whether the entity has been soft-deleted.
+    */
   type RecordStatus
 
+  /**
+    * Value that indicates an entity has *not* been soft-deleted.
+    */
   def active: RecordStatus
 
+  /**
+    * Value that indicates an entity has been soft-deleted.
+    */
   def deleted: RecordStatus
 
+  /**
+    * Column definition of the record status field in the entity.
+    */
   def $recordStatus(table: EntityTable): Rep[RecordStatus]
 
+  // Override the delete behaviour to soft-delete via an update.
   override def deleteById(id: Id)(implicit exc: ExecutionContext): DBIO[Int] =
     filterById(id).map($recordStatus).update(deleted)
 
-  def activeRecordsFilter(table: EntityTable): Rep[Boolean] = $recordStatus(table) === active
-
-  override def baseQuery: Query[EntityTable, Entity, Seq] = super.baseQuery.filter(activeRecordsFilter)
+  // Override the base query to only return entities that have not been soft-deleted.
+  override def baseQuery: Query[EntityTable, Entity, Seq] = super.baseQuery.filter($recordStatus(_) === active)
 }
